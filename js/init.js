6d8ed6fdc9abb06412177668e8a753e469368cee
Isaabomohana <mrissaam@outlook.com>
2017-02-08 19:17:46 +0100

    fix confliict2

--- js/init.js
+++ js/init.js
@@ -1,19 +1,3 @@
-/*scroll down*/ 
-$(function() {
-  $('a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html, body').stop().animate({
-          scrollTop: target.offset().top 
-        }, 1000);
-        return false;
-      }
-    }
-  });
-});
-
 (function($){
   $(function(){
 
@@ -21,21 +5,4 @@ $(function() {
     $('.parallax').parallax();
 
   }); // end of document ready
-      // Highlight the top nav as scrolling occurs
-    $('body').scrollspy({
-        target: '.navbar-fixed-top',
-        offset: 51
-    });
-
-    // Closes the Responsive Menu on Menu Item Click
-    $('.navbar-collapse ul li a').click(function(){ 
-            $('.navbar-toggle:visible').click();
-    });
-
-    // Offset for Main Navigation
-    $('#mainNav').affix({
-        offset: {
-            top: 100
-        }
-    })
 })(jQuery); // end of jQuery name space
