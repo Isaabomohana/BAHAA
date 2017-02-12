754be1fa6e8dbe5a1f0f4eb204f3a70bf699bc15
wissam1234 <wissam.ghozlan@gmail.com>
2017-02-12 13:32:33 +0100

    scroll to top button function

--- js/init.js
+++ js/init.js
@@ -27,10 +27,38 @@ $(function() {
       inDuration: 500,
       outDuration: 500,
       hover: true, // Activate on hover
-      gutter: 140, // Spacing from edge
+      gutter: 93, // Spacing from edge
       belowOrigin: false, // Displays dropdown below the button
-      alignment: 'left', // Displays dropdown with edge aligned to the left of button
+      alignment: 'right', // Displays dropdown with edge aligned to the left of button
       stopPropagation: false // Stops event propagation
     }
   );
-       
+
+  // Show sideNav
+  $('.button-collapse').sideNav('show');
+  // Hide sideNav
+  $('.button-collapse').sideNav('hide');
+  // Destroy sideNav
+  $('.button-collapse').sideNav('destroy');
+
+  //scroll to top button
+$(document).ready(function(){
+     $(window).scroll(function () {
+            if ($(this).scrollTop() > 50) {
+                $('#back-to-top').fadeIn();
+            } else {
+                $('#back-to-top').fadeOut();
+            }
+        });
+        // scroll body to 0px on click
+        $('#back-to-top').click(function () {
+            $('#back-to-top').tooltip('hide');
+            $('body,html').animate({
+                scrollTop: 0
+            }, 800);
+            return false;
+        });
+        
+        $('#back-to-top').tooltip('show');
+
+});
