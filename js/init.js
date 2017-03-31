99687461c699525e09dd792e64f7752edcfffc08
wissam1234 <wissam.ghozlan@gmail.com>
2017-03-31 14:04:25 +0200

    page3 adds new card design

--- js/init.js
+++ js/init.js
@@ -1,6 +1,7 @@
+
 $(document).ready(function(){
-    $('.parallax').parallax();
-    $('.button-collapse').sideNav();
+  $('.parallax').parallax();
+  $('.button-collapse').sideNav();
 });
 
 
@@ -21,37 +22,37 @@ $(function() {
   });
 });
 
-  $('.dropdown-button').dropdown({
-      inDuration: 500,
-      outDuration: 500,
+$('.dropdown-button').dropdown({
+  inDuration: 500,
+  outDuration: 500,
       hover: false, // Activate on hover
       gutter: 93, // Spacing from edge
       belowOrigin: false, // Displays dropdown below the button
       alignment: 'right', // Displays dropdown with edge aligned to the left of button
       stopPropagation: false // Stops event propagation
     }
-  );
+    );
   //scroll to top button
-$(document).ready(function(){
-     $(window).scroll(function () {
-            if ($(this).scrollTop() > 50) {
-                $('#back-to-top').fadeIn();
-            } else {
-                $('#back-to-top').fadeOut();
-            }
-        });
+  $(document).ready(function(){
+   $(window).scroll(function () {
+    if ($(this).scrollTop() > 50) {
+      $('#back-to-top').fadeIn();
+    } else {
+      $('#back-to-top').fadeOut();
+    }
+  });
         // scroll body to 0px on click
         $('#back-to-top').click(function () {
-            $('#back-to-top').tooltip('hide');
-            $('body,html').animate({
-                scrollTop: 0
-            }, 800);
-            return false;
+          $('#back-to-top').tooltip('hide');
+          $('body,html').animate({
+            scrollTop: 0
+          }, 800);
+          return false;
         });
         
         $('#back-to-top').tooltip('show');
 
-});
+      });
 
 
   // Show sideNav
