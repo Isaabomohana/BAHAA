bae04ad7c3a617cf7b148d707c03ec8e4c731a27
Isaabomohana <mrissaam@outlook.com>
2017-06-13 16:51:41 +0200

    remove icons from collapsable-header, fix init.js and correct images url

--- assets/js/init.js
+++ assets/js/init.js
@@ -97,15 +97,12 @@
 })(jQuery); // end of jQuery name space
 
    jQuery(document).ready(function() {
-      jQuery('.post').addClass("hidden").viewportChecker({
-          classToAdd: 'visible animated fadeInDown', // Class to add to the elements when they are visible
-          offset: 100
-         });
-    });
-
-   jQuery(document).ready(function() {
       jQuery('.post2').addClass("hidden").viewportChecker({
           classToAdd: 'visible animated fadeInUp', // Class to add to the elements when they are visible
           offset: 100
          });
+     jQuery('.post').addClass("hidden").viewportChecker({
+         classToAdd: 'visible animated fadeInDown', // Class to add to the elements when they are visible
+         offset: 100
+        });
     });
